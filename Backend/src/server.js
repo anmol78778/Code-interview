2dd25dbc6f2ef1d9ef330c58fef59f6d0e731472
Anmol Kumar <anmolkroe@gmail.com>
2026-06-03 01:14:14 +0530

    Implement MongoDB connection and integrate Clerk for authentication

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 
 const app = express(); 
@@ -7,10 +8,29 @@ const app = express();
 console.log(ENV.PORT)
 
 
+
 app.get('/health',(req,res)=>{
     res.status(200).json({message:'Server is healthy'})
 })
 
-app.listen(ENV.PORT,()=>{
-    console.log(`Server is running on port ${ENV.PORT} `);
-})
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
+
+
+// app.listen(ENV.PORT, () => {
+//     console.log(`Server is running on port ${ENV.PORT}`);
+//     console.log("About to call connectDB");
+//     connectDB();
+// });
+
+
