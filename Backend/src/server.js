f4e06c0a98f78e032b9aca67932f787e989c2786
Anmol Kumar <anmolkroe@gmail.com>
2026-06-03 14:45:20 +0530

    feat: implement chat token generation and protect routes with Clerk authentication

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,25 +4,33 @@ import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import {serve} from 'inngest/express'
 import { inngest, functions } from "./lib/inngest.js";
+// import {protectRoute} from './middleware/protectRoute.js';
+import chatRoutes from './routes/chatRoutes.js';
+
+// import sessionRoutes from './routes/sessionRoutes.js';
+import {clerkMiddleware} from '@clerk/express'
 const app = express(); 
 
 console.log(ENV.PORT)
 
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL ,credentials: true }));
+app.use(clerkMiddleware());
+// console.log(process.env.STREAM_API_KEY);
+// console.log(process.env.STREAM_API_SECRET);
 
-console.log(process.env.STREAM_API_KEY);
-console.log(process.env.STREAM_API_SECRET);
+// app.get("/", (req, res) => {
+//   res.send("Backend working");
+// });
 
-app.get("/", (req, res) => {
-  res.send("Backend working");
-});
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+// app.use("/api/sessions", sessionRoutes);
 
-
-app.get('/health',(req,res)=>{
-    res.status(200).json({message:'Server is healthy'})
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
 
 
