2cfbc30bb248f331294e401966fd32306dafeda1
Anmol Kumar <anmolkroe@gmail.com>
2026-06-03 12:39:44 +0530

    feat: integrate Inngest for user synchronization and deletion
    - Added Inngest as a dependency to handle serverless functions.
    - Created new functions to sync users on creation and delete users on deletion from Clerk.
    - Introduced a User model to store user data in MongoDB.
    - Updated environment variables to include keys for Inngest and Clerk.
    - Configured CORS in the server to allow requests from the client URL.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
-
-
+import cors from 'cors';
+import {serve} from 'inngest/express'
+import { inngest, functions } from "./lib/inngest.js";
 const app = express(); 
 
 console.log(ENV.PORT)
 
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL ,credentials: true }));
+
+
+// app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get('/health',(req,res)=>{
